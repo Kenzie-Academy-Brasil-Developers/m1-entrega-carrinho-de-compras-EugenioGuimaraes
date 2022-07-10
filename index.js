bc4ad95089c7023d2191c741056f35e12a3c3c52
Eugenio Lobo Guimaraes <eugenio.guimaraes1112@gmail.com>
2022-07-10 18:56:01 -0300

    atuallizações

--- index.js
+++ index.js
@@ -1,29 +1,63 @@
 
-let x = `
-<header>
-</header>
-<main>
-    <section class="container">
-        <div class="cabecalho">
-            <span>Total</span>
-            <span>Valor</span>
-        </div>
-        <ul class="lista__carrinho">
-        </ul>
-        <div class="subtotal">
-            <div class="mensagem">
-                <span>Subtotal</span>
-                <span>valor</span>
-            </div>
-            <button type="submit" name="finalizar">Finalizar compra</button>
-        </div>
-    </section>
-</main>
-`
-let body = document.querySelector('body');
-body.innerHTML = x;
+// let x = `
+// <header>
+// </header>
+// <main>
+//     <section class="container">
+//         <div class="cabecalho">
+//             <span>Total</span>
+//             <span>Valor</span>
+//         </div>
+//         <ul class="lista__carrinho">
+//         </ul>
+//         <div class="subtotal">
+//             <div class="mensagem">
+//                 <span>Subtotal</span>
+//                 <span>valor</span>
+//             </div>
+//             <button type="submit" name="finalizar">Finalizar compra</button>
+//         </div>
+//     </section>
+// </main>
+// `
+// let body = document.querySelector('body');
+// body.innerHTML = x;
 
-const produtos = [];
+// const produtos = [];
+
+
+
+let body = document.querySelector('body');
+
+let header = document.createElement('header');
+
+let principal = document.createElement('main');
+
+let container = document.createElement('section');
+container.setAttribute('class', 'container');
+
+let divCabe = document.createElement('div');
+divCabe.setAttribute('class', 'cabecalho');
+
+let divCabeSpan1 = document.createElement('span');
+divCabeSpan1.innerText = 'Total';
+
+let divCabeSpan2 = document.createElement('span');
+divCabeSpan1.innerText = 'Valor';
+
+let tagUl = document.createElement('ul');
+tagUl.setAttribute('class', 'lista__carrinho');
+
+let subtotal = document.createElement('div');
+subtotal.setAttribute('class', 'subtotal');
+
+let mensagemSubtotal = document.createElement('div');
+subtotal.setAttribute('class', 'mensagem');
+
+
+body.append(header)
+body.append(principal)
+
 
 
 
